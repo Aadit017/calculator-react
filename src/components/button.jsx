bf21e2eb39e3d332a33852a9e060fbeed48822fc
Aadit <baggaaadit017@gmail.com>
2021-03-24 18:32:20 +0530

    stylign left

---
+++ src/components/button.jsx
@@ -0,0 +1,37 @@
+import React ,{useState} from 'react'
+import "./button.style.scss"
+
+export default function Button() {
+    const buttonPressed=(number)=>{
+            console.log(number);
+            setResult(result+number)
+    }
+    const equal =()=>{
+        // eslint-disable-next-line no-eval
+        setResult(eval(result));
+    }
+    const clear=()=>{
+        setResult(0)
+    }
+    const [result ,setResult ]=useState("")
+    return (
+        <div>
+            <h1>{result}</h1>
+            <button onClick={()=>buttonPressed("1")}> 1 </button>
+            <button onClick={()=>buttonPressed("2")}> 2 </button>
+            <button onClick={()=>buttonPressed("3")}> 3 </button>
+            <button onClick={()=>buttonPressed("4")}> 4 </button>
+            <button onClick={()=>buttonPressed("5")}> 5 </button>
+            <button onClick={()=>buttonPressed("6")}> 6 </button>
+            <button onClick={()=>buttonPressed("7")}> 7 </button>
+            <button onClick={()=>buttonPressed("8")}> 8 </button>
+            <button onClick={()=>buttonPressed("9")}> 9 </button>
+            <button onClick={()=>buttonPressed("+")}> + </button>
+            <button onClick={()=>buttonPressed("-")}> - </button>
+            <button onClick={()=>buttonPressed("/")}> / </button>
+            <button onClick={()=>buttonPressed("*")}> * </button>
+            <button onClick={()=>equal("=")}> = </button>
+            <button onClick={()=>clear()}> CLEAR </button>
+        </div>
+    )
+}
--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,39 +1,17 @@
 import React, { Component } from "react"
 import "./calculator.style.scss"
-
+import Button from "./button"
 class Calculator extends Component{
     // eslint-disable-next-line no-useless-constructor
     constructor(){
         super();
-        this.state={
-        calculation: " ",
-        valu:"_"
-        }
-}
-handleChange=event=>{
-    const {name,value}=event.target;
-    this.setState({[name]:value})
-    
-    console.log(this.state.valu);
-}
-handleSubmit=event=>{
-    console.log(this.state.calculation);
-    event.preventDefault();
 }
 render(){
-    return(
-        <div className="container">
-        <h1>{this.state.valu}</h1>
-        <form onClick={this.handlesubmit}>
-            <input type="text"
-                name="emptyFeild"
-                value={this.state.calculation}
-                onChange={this.handleChange}
-            />
-        </form>
+    return (
+        <div>
+            <Button />
         </div>
-        
     )
 }
 }
-export default Calculator
+export default Calculator;
